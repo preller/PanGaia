4efc5e8a2ed6912726abecdb766c7abcef183de0
Héctor Cánovas <47888071+hectorcanovas@users.noreply.github.com>
2019-10-25 15:29:40 +0200

    Add files via upload

---
+++ library/lib_gaia_query.py
@@ -0,0 +1,150 @@
+""""
+Dedicated Class to query the Gaia archive
+
+Héctor Cánovas May 2019 - now
+"""
+import warnings, glob
+import numpy as np
+from astropy.utils.exceptions import AstropyWarning
+from astroquery.gaia          import Gaia
+
+# Extra Methods ================================
+def read_float_input(text = 'Introduce Input: '):
+    while True:
+        try:
+            read_val = np.float(input(text))
+            break
+        except ValueError:
+            print('This is not a number. Try again...')
+
+    return read_val
+
+
+def make_col_str(inp_list, first_label = ''):
+    out_cols   = [first_label + inp + ',' for inp in inp_list]
+    out_cols   = " ".join(out_cols)
+    out_cols   = out_cols[:-1]  
+    return out_cols
+
+
+class LibGaiaQuery():
+    """
+    Initialize the class.
+    """
+    def __init__(self):
+        """
+        Define catalogue/column attributes
+        """
+        # Select Gaia DR2 Cols ===============
+        cols_astrom    = ['source_id', 'ra', 'dec', 'parallax', 'parallax_error', 'pmra', 'pmra_error', 'pmdec', 'pmdec_error'] # Astrometry
+        cols_phot_m    = ['phot_' +  inp + '_mean_mag'             for inp in ['g', 'bp', 'rp']]                                # Photometry [mags]
+        cols_phot_f    = ['phot_' +  inp + '_mean_flux_over_error' for inp in ['g', 'bp', 'rp']]                                # Photometry [Flux]
+        cols_phot_o    = ['l', 'b', 'visibility_periods_used', 'radial_velocity']                                               # Miscellaneus
+
+        # Define WISE Cols ==============
+        wise_cols      = ['ph_qual', 'ra as Wra', 'dec as Wdec', 'w1mpro as W1mag', 'w2mpro as W2mag',
+                             'w3mpro as W3mag', 'w4mpro as W4mag', 'w1mpro_error as e_W1mag',
+                             'w2mpro_error as e_W2mag', 'w3mpro_error as e_W3mag', 'w4mpro_error as e_W4mag']
+        # Define 2MASS Cols =============
+        tmass_cols     = ['ph_qual', 'j_m as Jmag', 'h_m as Hmag','ks_m as Kmag', 'j_msigcom as e_Jmag', 
+                             'h_msigcom as e_Hmag', 'ks_msigcom as e_Kmag']
+
+        self.gaia_cols   = make_col_str(cols_astrom + cols_phot_m + cols_phot_f+ cols_phot_o, first_label = 'gaia.')
+        self.gaia_cols_p = make_col_str(['source_id'] + cols_phot_m + cols_phot_f, first_label='gaia.')
+        self.wise_cols   = make_col_str(wise_cols,  first_label = 'wise.')
+        self.tmass_cols  = make_col_str(tmass_cols, first_label = 'tmass.')
+
+
+    def __repr__(self):
+        """Return developer-friendly string representation."""
+        return f'Class to create the Gaia/Analysis sample'
+
+
+    def set_cone_search_pars(self, sample_name = None, ra = None, dec = None, radius = None, para_min = None, para_max = None):
+        """
+        Prepare cone search parameters.
+        """
+        text_0 = 'Preparing Cone-Search ADQL query. Introduce '
+        if not sample_name: sample_name = input('Introduce sample name (e.g. IC_348_test): ')
+        if not ra:          ra          = read_float_input(text_0 + 'R.A.   [decimal degree]: ')
+        if not dec:         dec         = read_float_input(text_0 + 'Dec    [decimal degree]: ')
+        if not radius:      radius      = read_float_input(text_0 + 'radius [decimal degree]: ')
+        if not para_min:    para_min    = read_float_input(text_0 + 'min-Parallax [mas]: ')
+        if not para_max:    para_max    = read_float_input(text_0 + 'max-Parallax [mas]: ')
+        print()
+        self.sample_name = sample_name
+        self.ADQL_pars   = {'ra':ra, 'dec':dec, 'radius':radius, 'para_min':para_min, 'para_max':para_max}
+
+
+    def run_cone_search(self, quality_par_SN  = '10', quality_par_vis = '7',quality_par_ruwe = '1.40', verbose = True):
+        """
+        Run a Cone Search ADQL query on the Gaia DR2 archive. By default the code applies a selection criteria: objets with
+        parallax S/N <10, visibility periods used < 7, and RUWE >1.40 are excluded.
+        """
+        # 1.- Define ADQL query ================================
+        query  = ("SELECT " + self.gaia_cols + " "
+                ",sqrt(gaia.astrometric_chi2_al/(gaia.astrometric_n_good_obs_al-5)) as unit_weight_e, g_ruwe.ruwe as ruwe "
+                "FROM gaiadr2.gaia_source as gaia "
+                "LEFT OUTER JOIN gaiadr2.ruwe  AS g_ruwe ON gaia.source_id = g_ruwe.source_id "
+                "WHERE 1=CONTAINS( "
+                "POINT('ICRS',ra,dec), "
+                f"CIRCLE('ICRS',{self.ADQL_pars['ra']:5.2F}, {self.ADQL_pars['dec']:5.2F}, {self.ADQL_pars['radius']:5.2F})) "
+                f"AND parallax >= {self.ADQL_pars['para_min']:5.2F} AND parallax <= {self.ADQL_pars['para_max']:5.2F} "
+                f"AND gaia.source_id IS NOT NULL AND gaia.parallax/gaia.parallax_error >{quality_par_SN} "
+                f"AND gaia.visibility_periods_used >{quality_par_vis} AND g_ruwe.ruwe <{quality_par_ruwe}")
+        # 2.- Run ADQL query ===================================
+        warnings.simplefilter('ignore', category=AstropyWarning)
+        print(f'RUNNING ADQL ASYNCRHRONOUS QUERY ' + '=' * 57)
+        job       = Gaia.launch_job_async(query= query, verbose=True)
+        self.cat  = job.get_results()
+
+        flag_psn, flag_vis, flag_ruwe  = '', '', ''
+        if quality_par_SN == '10':     flag_psn  = '(Default)'
+        if quality_par_vis == '7':     flag_vis  = '(Default)'
+        if quality_par_ruwe == '1.40': flag_ruwe = '(Default)'        
+
+        if verbose:
+            print('=' * 90)
+            print(f'Selection Criteria in Parallax S/N:             Parallax S/N > {quality_par_SN}   {flag_psn}')
+            print(f'Selection Criteria in Visibility Periods Used:  Vis          > {quality_par_vis}    {flag_vis}')
+            print(f'Selection Criteria in RUWE:                     RUWE         < {quality_par_ruwe} {flag_ruwe}')
+            print()            
+            print(f'SAMPLE OUTPUT  N_els = {len(self.cat):3.0f}')
+            print('=' * 90)
+
+
+    def run_phot_crossmatch(self, upload_table = 'dummy.vot', verbose = True, add_input_cols = ""):
+        """
+        Run a crossmatch between Gaia, 2MASS and WISE using Gaia internal cross-matched tables.
+        """
+        # 1.- Search input table =============================== 
+        upload_resource   = glob.glob(upload_table)
+        upload_table_name = "input_table"
+        if len(upload_resource) == 0:
+            raise Exception('Upload Table not found')
+
+        if add_input_cols != "":
+            add_input_cols = "," + add_input_cols + " "
+
+        # 1.- Write ADQL query =================================
+        query  = ("SELECT " + self.gaia_cols_p + ", " + self.wise_cols + ", " + self.tmass_cols + " " + 
+                add_input_cols + 
+                "FROM tap_upload.input_table as input_table "
+                "LEFT OUTER JOIN gaiadr2.gaia_source as gaia ON input_table.source_id = gaia.source_id "           # Inp Sample VS Gaia DR2
+                "LEFT OUTER JOIN gaiadr2.allwise_best_neighbour as xmatch ON gaia.source_id = xmatch.source_id "   # Inp Sample VS WISE
+                "LEFT OUTER JOIN gaiadr1.allwise_original_valid as wise ON xmatch.allwise_oid = wise.allwise_oid " # Inp Sample VS WISE          
+                "LEFT OUTER JOIN gaiadr2.tmass_best_neighbour as xmatch_2 ON gaia.source_id = xmatch_2.source_id " # Inp Sample VS 2MASS
+                "LEFT OUTER JOIN gaiadr1.tmass_original_valid as tmass ON xmatch_2.tmass_oid = tmass.tmass_oid "   # Inp Sample VS 2MASS
+                f"WHERE cc_flags = '0000' "
+                f"AND ext_flag < 2 "
+                f"AND w3mpro_error IS NOT NULL "
+                f"AND w4mpro_error IS NOT NULL")
+        # 2.- Run ADQL query ===================================
+        print(f'RUNNING ADQL SYNCRHRONOUS QUERY ' + '=' * 57)
+        job      = Gaia.launch_job(query= query, upload_resource = upload_resource[0], upload_table_name = upload_table_name, verbose = True)
+        self.cat = job.get_results()
+
+        if verbose:
+            print('=' * 90)
+            print(f'SAMPLE OUTPUT  N_els = {len(self.cat):3.0f}')
+            print('=' * 90)                     
---
+++ library/lib_interactive.py
@@ -0,0 +1,266 @@
+'''
+Created on 26 August 2019
+
+See also: 
+https://github.com/jonmmease/plotly_ipywidget_notebooks/blob/master/notebooks/iris_dashboard.ipynb
+
+@author: hcanovas
+'''
+
+import pandas   as pd
+import numpy    as np
+import copy
+import plotly.graph_objs as go
+from plotly.graph_objs import FigureWidget
+from ipywidgets import HBox, VBox, Button
+import webcolors
+
+
+# Extra Methods ================================
+def text_annote(text = 'dummy', x = 0., y = 0., font_size = 12, textangle = 0., showarrow = False, xref = 'paper', yref = 'paper'):
+	"""
+	Used by "Interactive.explore_and_select()"
+	"""
+	return go.layout.Annotation(text = text, x = x, y = y, showarrow = showarrow, font_size = font_size,
+								xref = xref, yref = yref, textangle = textangle)
+
+def color_nm_to_rgba(color = 'black', alpha = 0.5):
+	ncolor = webcolors.name_to_rgb(color)
+	return f'rgba({ncolor[0]},{ncolor[1]},{ncolor[2]},{alpha})'
+
+
+
+class LibInteractive():
+	"""
+	Initialize the class.
+	"""
+	def __init__(self):
+		pass
+
+
+	def __repr__(self):
+		"""Return developer-friendly string representation."""
+		return f'Class to interactively explor Gaia DR2 data. Input object must be a library_gaia.cat object catalogue'
+
+
+	def load_gaia_cat(self, inp_cat):
+		"""
+		Load .vot table and export it to Pandas
+		"""
+		if inp_cat.__module__ == 'astropy.table.table':
+			print('Exporting Astropy Table to Pandas')
+			inp_cat = inp_cat.copy()
+			inp_cat = inp_cat.to_pandas()
+		self.cat = inp_cat
+
+
+	def load_control_obj(self, control):
+		"""
+		Load control *object*. This includes catalogue, label, and color.
+		"""
+		self.control     = copy.deepcopy(control)
+		if self.control.cat.__module__ == 'astropy.table.table':
+			print('Exporting Control Table to Pandas')
+			self.control.cat = self.control.cat.to_pandas()
+		
+
+	def set_marker(self, color_def = 'blue', color_high = 'red', size = 8, border_width = 1, border_color = 'black', 
+		show_scale = False, hist_marker = False, opacity = 1.0):
+		"""
+		Define generic Marker properties. It uses detault plotLy muted blue color (https://stackoverflow.com/questions/40673490/how-to-get-plotly-js-default-colors-list)
+		"""
+		color_def   = color_nm_to_rgba(color = color_def, alpha = 0.5)
+		colorscale  = [[0, color_def], [0.5, color_def],[0.5, color_high], [1, color_high]]    # 2 colors for Default & Selected
+		color       = np.zeros(len(self.cat))
+		self.marker = {'cmax': 1.5, 'cmin': -0.5,'color': color,'colorscale': colorscale,'showscale': show_scale,
+		               'line': dict(width = 1, color = 'black')} # Histogram layout is fixed
+
+		if hist_marker == False:
+			self.marker.update({'size':size, 'opacity':opacity, 'line':dict(width = border_width, color = border_color)})
+
+
+	def set_marker_control(self): 
+		"""
+		Define properties of the control sample markers.
+		"""
+		self.marker_control = {'color': self.control.color, 'size': self.marker['size'] * 0.75, 'line': dict(width = 0)}
+
+
+	def set_margins(self, fig, margins = None):
+		"""
+		Set Figure margins
+		"""
+		if margins:
+			if 'l' in margins.keys(): fig.update_layout(margin = go.layout.Margin(l = margins['l']))
+			if 'r' in margins.keys(): fig.update_layout(margin = go.layout.Margin(r = margins['r']))
+			if 't' in margins.keys(): fig.update_layout(margin = go.layout.Margin(t = margins['t']))
+			if 'b' in margins.keys(): fig.update_layout(margin = go.layout.Margin(b = margins['b']))
+		return fig
+
+
+	def set_fig_show(self, fig, margins = None, annotations = None, new_xrange = None, title = None):
+		"""
+		Set common Figure properties.
+		NOTE: https://plot.ly/python/renderers/   >> "(...), so you should not use the .show figure method or the plotly.io.show function on FigureWidget objects."
+		"""
+		if new_xrange:
+			fig.update_xaxes(range=new_xrange)
+		if annotations:
+			fig.update_layout(annotations = annotations)
+		if title:
+			if isinstance(title, str):
+				title = dict(text = title, y = 0.9)
+			fig.update_layout(title = go.layout.Title(text = title['text'], xref = 'paper', x = 0.5, y = title['y']))
+		if margins: 
+			fig = self.set_margins(fig, margins = margins)
+		return fig
+
+
+	def show_sky_coords(self, font_size = 16, width = 500, height = 500, y_xaxis = -0.15, x_yaxis = -0.2, title = None,
+		margins = None, **kwargs):
+		"""
+		Interactive RA-DEC plot
+		"""
+		self.set_marker(**kwargs)
+
+		fig = FigureWidget(**{
+		    'data': [{'x': self.cat.ra,'y': self.cat.dec,'marker': self.marker,'mode': 'markers','type': 'scatter'}],
+		    'layout': {'dragmode': 'pan', 'width': width, 'height': height, 'font_size':font_size} })
+
+		xtit = text_annote(text = '$\\text{R.A [}^\circ]$', x = 0.5,    y = y_xaxis, font_size = font_size * 1.2)
+		ytit = text_annote(text = '$\\text{Dec [}^\circ]$', x = x_yaxis, y = 0.5,    font_size = font_size * 1.2, textangle = -90)
+		fig  = self.set_fig_show(fig, margins = margins, annotations = [xtit, ytit], title = title, new_xrange = [self.cat.ra.max(), self.cat.ra.min()])
+
+		if hasattr(self, 'control'):
+			self.set_marker_control()
+			fig.add_trace(go.Scatter(x = self.control.cat.ra, y = self.control.cat.dec, marker = self.marker_control, mode = 'markers'))
+			fig.update_layout(showlegend=False)
+		self.fig_sky = fig
+
+
+	def show_properm(self, font_size = 16, width = 500, height = 500, y_xaxis = -0.15, x_yaxis = -0.2, title = None,
+		margins = None, **kwargs):
+		"""
+		Interactive Proper-Motions plot
+		"""
+		self.set_marker(**kwargs)
+
+		fig = FigureWidget(**{
+		    'data': [{'x': self.cat.pmra, 'y': self.cat.pmdec, 'marker': self.marker, 'mode': 'markers', 'type': 'scatter'}],
+		    'layout': {'dragmode': 'pan', 'width': width, 'height': height, 'font_size':font_size} })
+
+		xtit = text_annote(text = '$\\text{pmra [mas yr}^{-1}]$',  x = 0.5,     y = y_xaxis, font_size = font_size * 1.2)
+		ytit = text_annote(text = '$\\text{pmdec [mas yr}^{-1}]$', x = x_yaxis, y = 0.5,     font_size = font_size * 1.2, textangle = -90)
+		fig  = self.set_fig_show(fig, margins = margins, annotations = [xtit, ytit], title = title)
+		if hasattr(self, 'control'):
+			self.set_marker_control()
+			fig.add_trace(go.Scatter(x = self.control.cat.pmra, y = self.control.cat.pmdec, marker = self.marker_control, mode = 'markers'))
+			fig.update_layout(showlegend=False)
+		self.fig_properm = fig
+
+
+	def show_hist_dist(self, font_size = 16, width = 500, height = 500, y_xaxis = -0.15, x_yaxis = -0.2, title = None, 
+		margins = None,**kwargs):
+		"""
+		Interactive Histogram (distances) plot
+		"""
+		self.set_marker(hist_marker = True, **kwargs)
+
+		fig = FigureWidget(**{
+		    'data': [{'x': self.cat.distance, 'marker': self.marker, 'type': 'histogram'}],
+		    'layout': {'dragmode': 'pan', 'width': width, 'height': height, 'font_size':font_size} })
+
+		xtit = text_annote(text='Distance [pc]',  x = 0.5,     y = y_xaxis, font_size = font_size * 1.2)
+		ytit = text_annote(text='# Objects',      x = x_yaxis, y =  0.5,    font_size = font_size * 1.2, textangle = -90)
+		fig  = self.set_fig_show(fig, margins = margins, annotations = [xtit, ytit], title = title)
+		if hasattr(self, 'control'):
+			marker_control_hist          = self.marker.copy()
+			marker_control_hist['color'] = self.control.color
+			fig.add_trace(go.Histogram(x = self.control.cat.distance, marker = marker_control_hist))
+			fig.update_layout(barmode = 'overlay', showlegend=False)
+		self.fig_hist_dist = fig
+
+
+	def explore_and_select(self, color_high  = 'red', font_size = 16, height = 500, marker_size = 8, **kargs):
+		"""
+		ALL-IN-ONE analysis window
+		"""
+		margins = dict(t = 80)
+		width   = 500
+		self.show_sky_coords(width = width, height = height, size = marker_size, color_high = color_high, margins = margins, y_xaxis = -0.15, title = dict(text = 'Sky Position',          y = 0.9), **kargs)
+		self.show_properm(   width = width, height = height, size = marker_size, color_high = color_high, margins = margins, y_xaxis = -0.15, title = dict(text = 'Projected Velocity',    y = 0.9), **kargs)
+		self.show_hist_dist( width = width, height = height, size = marker_size, color_high = color_high, margins = margins, y_xaxis = -0.15, title = dict(text = 'Distance Distribution', y = 0.9), x_yaxis = -0.15, **kargs)
+
+		f1 = self.fig_sky
+		f2 = self.fig_properm
+		f3 = self.fig_hist_dist
+
+		scatt1 = f1.data[0]
+		scatt2 = f2.data[0]
+		hist_3 = f3.data[0]
+
+		# Interactive data selection =======================
+		def brush(trace, points, state):
+			inds = np.array(points.point_inds)
+			if inds.size:
+				self.cat_subsamp      = self.cat.loc[inds,:]       # Selected sample
+				selected              = scatt1.marker.color.copy()
+				selected[inds]        = 1
+				scatt1.marker.color   = selected
+				scatt2.marker.color   = selected
+
+				#  Histograms require a bit of care:
+				self.set_marker(hist_marker = True, color_def = color_high)
+				marker          = self.marker
+				marker['color'] = color_high
+				self.fig_hist_dist.add_trace(go.Histogram(x = self.cat.distance[inds], marker = self.marker))
+				self.fig_hist_dist.update_layout(barmode = 'overlay', showlegend=False)
+
+		scatt1.on_selection(brush)
+		scatt2.on_selection(brush)
+		# Reset Interactive Selection ======================
+		def reset_brush(btn):
+			selected            = np.zeros(len(self.cat))
+			scatt1.marker.color = selected
+			scatt2.marker.color = selected
+
+			 # Remove sub-sample histogram
+			if hasattr(self, 'control'): 
+				f3.data = [f3.data[0], f3.data[1]]
+			else:
+				f3.data = [f3.data[0]]
+			delattr(self, "cat_subsamp")
+
+		# Create reset button ==============================		    
+		button = Button(description="clear")
+		button.on_click(reset_brush)
+
+		# Widget ===============
+		self.dashboard    = VBox([HBox([f1, f2,f3]), button])
+
+
+	def show_3D_space(self, font_size = 16, width = 1200, height = 500, y_xaxis = -0.15, x_yaxis = -0.2, title = None,
+		margins = None, **kwargs):
+		"""
+		Interactive Proper-Motions plot
+		"""
+		self.set_marker(**kwargs)
+		
+		fig = FigureWidget(**{
+		    'data': [{'x': self.cat.X_gal, 'y': self.cat.Y_gal, 'z': self.cat.Z_gal, 'marker': self.marker, 'mode': 'markers', 'type': 'scatter3d'}],
+		    'layout': {'dragmode': 'pan', 'width': width, 'height': height, 'font_size':font_size, 'showlegend': False}})
+
+		if hasattr(self, 'control'):
+			self.set_marker_control()
+			fig.add_trace(go.Scatter3d(
+				{'x': self.control.cat.X_gal, 'y': self.control.cat.Y_gal, 'z': self.control.cat.Z_gal, 'mode': 'markers', 'marker':self.marker_control}
+				))
+
+		if hasattr(self, 'cat_subsamp'):
+			fig.add_trace(go.Scatter3d(
+				{'x': self.cat_subsamp.X_gal, 'y': self.cat_subsamp.Y_gal, 'z': self.cat_subsamp.Z_gal, 'mode': 'markers'}
+				))
+		
+		fig.update_layout(scene = dict(xaxis_title = 'X_Gal', yaxis_title = 'Y_Gal', zaxis_title = 'Z_Gal'))
+		fig = self.set_fig_show(fig, margins = margins, title = title)
+		self.fig_3D = fig
